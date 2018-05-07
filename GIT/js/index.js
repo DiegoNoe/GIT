b82dbc3d981501541a45c9a9502fdf33b24a1eaf
Diego Noe Vargas Ornelas <diegonoevo@Diegos-MacBook-Pro.local>
2018-05-07 16:09:00 -0500

    first commit

---
+++ GIT/js/index.js
@@ -0,0 +1,12 @@
+function connectFB() {
+    var config = {
+        apiKey: "AIzaSyDrVZ3499rhvfFpxfRbU-wqJ-ZUN0-_ez0",
+        authDomain: "cremeria-imperial.firebaseapp.com",
+        databaseURL: "https://cremeria-imperial.firebaseio.com",
+        projectId: "cremeria-imperial",
+        storageBucket: "cremeria-imperial.appspot.com",
+        messagingSenderId: "445465872507"
+    };
+
+    return config;
+}
